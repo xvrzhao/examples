03e36346bc9792c158d8322588d0c8de34bc1ee5
Xavier Zhao <xvrzhao@gmail.com>
2020-08-25 16:00:50 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,20 @@
+module jaeger-example
+
+go 1.15
+
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/go-playground/validator/v10 v10.3.0 // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/opentracing/opentracing-go v1.2.0
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/uber/jaeger-client-go v2.25.0+incompatible
+	github.com/uber/jaeger-lib v2.2.0+incompatible // indirect
+	go.uber.org/atomic v1.6.0 // indirect
+	golang.org/x/sys v0.0.0-20200821140526-fda516888d29 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
+)
---
+++ jaeger/http/main.go
@@ -0,0 +1,187 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
+	"github.com/uber/jaeger-client-go"
+	"github.com/uber/jaeger-client-go/config"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+// configurations
+const (
+	serverHost = "localhost"
+	serverPort = 6666
+
+	tracerAgentHost   = "localhost"
+	tracerAgentPort   = 32772
+	tracerServiceName = "myHTTPService"
+)
+
+var httpHost = fmt.Sprintf("http://%s:%d", serverHost, serverPort)
+
+func initTracer(serviceName, localAgentHost string, localAgentPort int) io.Closer {
+	cfg := config.Configuration{
+		ServiceName: serviceName,
+		Sampler:     &config.SamplerConfig{Type: jaeger.SamplerTypeConst, Param: 1},
+		Reporter:    &config.ReporterConfig{LocalAgentHostPort: fmt.Sprintf("%s:%d", localAgentHost, localAgentPort)},
+	}
+
+	tracer, closer, err := cfg.NewTracer()
+	if err != nil {
+		err = fmt.Errorf("initTracer: newTracer: %w", err)
+		log.Fatal(err)
+	}
+
+	opentracing.SetGlobalTracer(tracer)
+	return closer
+}
+
+// traceMiddleware retrieve spanContext from client request, if there is, start a span childOf it,
+// otherwise, start a new root span.
+func traceMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if opentracing.IsGlobalTracerRegistered() {
+			tracer := opentracing.GlobalTracer()
+			clientSpanCtx, err := tracer.Extract(opentracing.HTTPHeaders, opentracing.HTTPHeadersCarrier(c.Request.Header))
+			var (
+				span opentracing.Span
+				// note: the span operation name should be a general string to allow the tracing systems to do aggregations,
+				// so the request path should be a gin registered route path rather than a request URL, since a registered
+				// route path may contain path params like '/user/:id'.
+				spanOpName = fmt.Sprintf("%s %s", c.Request.Method, c.FullPath() /* not c.Request.URL */)
+			)
+			if err == nil { // has client span context, inherit it
+				span = tracer.StartSpan(spanOpName, opentracing.ChildOf(clientSpanCtx))
+			} else { // doesn't has, start a root span
+				span = tracer.StartSpan(spanOpName)
+			}
+			defer span.Finish()
+
+			// set span tag
+			ext.Component.Set(span, "gin")
+			ext.HTTPMethod.Set(span, c.Request.Method)
+			ext.HTTPUrl.Set(span, c.Request.URL.String())
+			span.SetTag("http.params", fmt.Sprint(c.Params))
+			span.SetTag("http.queries", fmt.Sprint(c.Request.URL.Query()))
+
+			// put the span in request-scope context of gin
+			c.Set("span", span)
+		}
+
+		c.Next()
+	}
+}
+
+// HTTPGetWithTrace inject this service scoped spanContext into carrier of outgoing HTTP request,
+// to allow the server to extract it from incoming request.
+func HTTPGetWithTrace(URL string, c *gin.Context) (body []byte, err error) {
+	if !opentracing.IsGlobalTracerRegistered() {
+		err = errors.New("HTTPGetWithTrace: global tracer is not registered")
+		return
+	}
+	tracer := opentracing.GlobalTracer()
+
+	v, exist := c.Get("span")
+	span, ok := v.(opentracing.Span)
+	if !exist || !ok {
+		err = errors.New("HTTPGetWithTrace: can't retrieve span from context")
+		return
+	}
+
+	req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, URL, nil)
+	if err != nil {
+		err = fmt.Errorf("HTTPGetWithTrace: can't new request: %w", err)
+		return
+	}
+
+	if err = tracer.Inject(span.Context(), opentracing.HTTPHeaders, opentracing.HTTPHeadersCarrier(req.Header)); err != nil {
+		err = fmt.Errorf("HTTPGetWithTrace: can't inject spanContext to carrier")
+		return
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		err = fmt.Errorf("HTTPGetWithTrace: can't do request: %w", err)
+		return
+	}
+	defer res.Body.Close()
+
+	if body, err = ioutil.ReadAll(res.Body); err != nil {
+		err = fmt.Errorf("HTTPGetWithTrace: can't read body: %w", err)
+		return
+	}
+
+	return
+}
+
+// handlers
+
+func fooHandler(c *gin.Context) {
+	barBody, err := HTTPGetWithTrace(httpHost+"/bar", c)
+	if err != nil {
+		c.String(http.StatusInternalServerError, "%d", http.StatusInternalServerError)
+		log.Print(err)
+		return
+	}
+
+	bazBody, err := HTTPGetWithTrace(httpHost+"/baz", c)
+	if err != nil {
+		c.String(http.StatusInternalServerError, "%d", http.StatusInternalServerError)
+		log.Print(err)
+		return
+	}
+
+	c.String(http.StatusOK, "%s\n%s\n", barBody, bazBody)
+}
+
+func barHandler(c *gin.Context) {
+	quxBody, err := HTTPGetWithTrace(httpHost+"/qux", c)
+	if err != nil {
+		c.String(http.StatusInternalServerError, "%d", http.StatusInternalServerError)
+		log.Print(err)
+		return
+	}
+	c.String(http.StatusOK, string(quxBody))
+}
+
+func bazHandler(c *gin.Context) {
+	c.String(http.StatusOK, "response from baz")
+}
+
+func quzHandler(c *gin.Context) {
+	c.String(http.StatusOK, "response from qux")
+}
+
+func main() {
+	closer := initTracer(tracerServiceName, tracerAgentHost, tracerAgentPort)
+	defer closer.Close()
+
+	r := gin.Default()
+	r.Use(traceMiddleware()) // use trace
+
+	// network tracing:
+	//     /foo
+	//       |
+	//    |------|
+	//   /bar  /baz
+	//    |
+	//   /qux
+
+	r.GET("/foo", fooHandler)
+	r.GET("/bar", barHandler)
+	r.GET("/baz", bazHandler)
+	r.GET("/qux", quzHandler)
+
+	if err := r.Run(":6666"); err != nil {
+		err = fmt.Errorf("main: run gin server: %w", err)
+		log.Fatal(err)
+	}
+}
