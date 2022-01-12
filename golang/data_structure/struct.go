2f3e64054af2c9ecad2f492bcb5162ce9eda2310
Xavier Zhao <xvrzhao@gmail.com>
2022-01-12 15:38:21 +0800

    add: RunStructPointerExample

--- golang/data_structure/struct.go
+++ golang/data_structure/struct.go
@@ -14,3 +14,29 @@ func RunStructAddressExample() {
 	fmt.Printf("address of p.Name:\t%p\n", &p.Name)
 	fmt.Printf("address of p.Age:\t%p\n", &p.Age)
 }
+
+func RunStructPointerExample() {
+	p1 := &Person{Name: "p1"}
+	p2 := &Person{Name: "p2"}
+
+	fmt.Printf("p1: %p %v\n", p1, p1)
+	fmt.Printf("p2: %p %v\n", p2, p2)
+
+	p1 = p2 // p1 所存指针改变，p1 与 p2 同时指向一块内存区域
+
+	fmt.Printf("p1: %p %v\n", p1, p1)
+	fmt.Printf("p2: %p %v\n", p2, p2)
+
+	// ---
+
+	p1 = &Person{Name: "p1"}
+	p2 = &Person{Name: "p2"}
+
+	fmt.Printf("p1: %p %v\n", p1, p1)
+	fmt.Printf("p2: %p %v\n", p2, p2)
+
+	*p1 = *p2 // p1 所存指针不变，仅仅将 p2 所指向的对象的值赋给 p1 所指向的对象
+
+	fmt.Printf("p1: %p %v\n", p1, p1)
+	fmt.Printf("p2: %p %v\n", p2, p2)
+}
